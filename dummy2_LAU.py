4b884818f19f6b2894c82c5f7f18ab2285c68109
Laurens Stuurman <laurenssturman678@hotmail.com>
2019-10-01 09:23:11 +0200

    changes to controllerlau and mutation fixed

--- dummy2_LAU.py
+++ dummy2_LAU.py
@@ -1,14 +1,10 @@
-################################
-# EvoMan FrameWork - V1.0 2016 #
-# Author: Karine Miras         #
-# karine.smiras@gmail.com      #
-################################
-
 # imports framework
 import sys, os
 sys.path.insert(0, 'evoman')
 from environment import Environment
-from demo_controller import player_controller
+from controller_lau1 import controller_automata
+from itertools import product
+from functools import reduce
 
 
 # import libraries
@@ -26,7 +22,7 @@ if not os.path.exists(experiment_name):
 # initializes environment with ai player using random controller, playing against static enemy
 env = Environment(experiment_name='saturday',
                   enemies=[2],
-                  player_controller=player_controller(),
+                  player_controller=controller_automata(),
                   level=2,
                   speed="fastest")
 
@@ -36,7 +32,24 @@ env = Environment(experiment_name='saturday',
 # set time marker
 ini = time.time()
 
-# define parameters
+
+
+def create_random_rulebook():
+    rulebook = {}
+    # list of keys : 
+    keys = list(product([0,1], repeat=20))
+    # possible outputs : 
+    outputs = list(product([0,1], repeat=5))
+
+    # convert to lists :
+    keys = [reduce(lambda x,y:str(x) + str(y),k) for k in keys]
+    outputs = [list(o) for o in outputs]
+    # iteratively create dict with random outputs: 
+    for key in keys:
+        ind = random.randint(0,24)
+        rulebook[key] = outputs[ind]
+    print('Creating Rulebook')
+    return rulebook
 
 
 def evaluate(pop):
@@ -48,8 +61,13 @@ def evaluate(pop):
     return fitness
 
 def crossover(agent_x, agent_y):
-    ind = np.random.randint(1,n_vars-2)
-    new_agent = np.concatenate((agent_x[:ind], agent_y[ind:]), axis=None)
+    new_agent = {}
+    len_dict = len(list(agent_x.values()))
+    keys = list(agent_x.keys())
+    ind = np.random.randint(1,len_dict)
+    other_controls = list(agent_x.values())[:ind] + list(agent_y.values())[ind:]
+    for i in range(len_dict):
+        new_agent[keys[i]] = other_controls[i]
     return new_agent
 
 def order_to_fitness(fitness,pop):
@@ -62,17 +80,17 @@ def make_children(pop, fitness, n_pop):
     fitness,pop = order_to_fitness(fitness,pop)
     chance = [(float(i)-fitness[-1]) / (fitness[0] - fitness[-1]) for i in fitness]
     cumchance = np.cumsum(chance)
-    n_kids = int(0.25 * n_pop)
+    n_kids = int(0.35 * n_pop)
     for i in range(n_kids):
         while True:
             first = random.uniform(0, cumchance[-1])
             second = random.uniform(0, cumchance[-1])
-            for i in range(len(cumchance)):
-                if first < cumchance[i]:
+            for j in range(len(cumchance)-1):
+                if first < cumchance[j]:
                     ind1 = i
                     break
-            for i in range(len(cumchance)):
-                if second < cumchance[i]:
+            for z in range(len(cumchance)-1):
+                if second < cumchance[z]:
                     ind2 = i
                     break
             if (ind1 != ind2):
@@ -84,14 +102,20 @@ def make_children(pop, fitness, n_pop):
 def mutate(kids,n_flip):
     to_mutate = int(mutation*len(kids))
     mutated_kids = []
+    #list of keys and possible outputes : 
+    keys = list(kids[0].keys())
+    outputs = list(product([0,1], repeat=5))
+    # to lists
+    outputs = [list(o) for o in outputs]
     for i in range(to_mutate):
         ind = np.random.randint(len(kids)-1)
         while ind in mutated_kids:
             ind = np.random.randint(len(kids)-1)
         for j in range(n_flip):
-            toflip = np.random.randint(n_vars)
+            toflip = np.random.randint(n_rules)
+            random_out = np.random.randint(24)
             #print('fitnes before mutation : ',evaluate([kids[ind]]))
-            kids[ind][toflip] = np.random.uniform(-1,1)
+            kids[ind][keys[toflip]] = outputs[random_out]
             #print('fitness after mutation : ',evaluate([kids[ind]]))
         mutated_kids.append(ind)
     return kids
@@ -116,17 +140,13 @@ def kill(pop,fitness,kids):
 
 if __name__ == "__main__":
     # params
-    n_hidden = 10
-    n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
     n_point_mut = 50
-    dom_u = 1
-    dom_l = -1
-    n_pop = 20
-    n_gen = 5
-    mutation = 0.2
-    last_best = 0
+    n_pop = 50
+    n_gen = 20
+    mutation = 0.5
+    n_rules = 1048576
 
-    pop = np.random.uniform(dom_l, dom_u, (n_pop, n_vars))
+    pop = [create_random_rulebook() for i in range(n_pop)]
     fitnesses = []
     best_fitness = []
 
@@ -144,10 +164,10 @@ if __name__ == "__main__":
         kids = mutate(kids,n_point_mut)
         # kill some peeps
         pop,fitness = kill(pop,fitness,kids)
-    outfile = "populations/first_test.p"
-    result_out = "results/best_fit1.p"
-    result_out2 = "results/fitness1.p"
-    pkl.dump(pop,open(outfile, 'wb'))
+    outfile = "populations/ConL_first_test.p"
+    result_out = "results/ConL_best_fit1.p"
+    result_out2 = "results/ConL_fitness1.p"
+    #pkl.dump(pop,open(outfile, 'wb'))
     pkl.dump(best_fitness,open(result_out,'wb'))
     pkl.dump(fitness,open(result_out2,'wb'))
 
--- dummy_demo_attempt_lau.py
+++ dummy_demo_attempt_lau.py
@@ -1,58 +1,15 @@
-################################
-# EvoMan FrameWork - V1.0 2016 #
-# Author: Karine Miras         #
-# karine.smiras@gmail.com      #
-################################
-
-print('poop')
-
-# imports framework
-import sys, os
-sys.path.insert(0, 'evoman')
-from environment import Environment
-
-print('poop')
-
-# import libraries
-import time
+from controller import Controller
 import numpy as np
-from math import fabs,sqrt
-import glob, os
-from controller_specialist_demo import player_controller
+import time
 
-print('poop')
+class controller_automata(Controller, rulebook):
 
-experiment_name = 'dummy_demo_lau'
-if not os.path.exists(experiment_name):
-    os.makedirs(experiment_name)
+    def __init__(self):
+        self.rulebook = rulebook
 
-def init_pop(pop_size, env):
-    # Function that initializes population in the same way as done in example (optimization_specialis.py)
-    n_hidden = 10
-    agent_variables = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
-    # Actual population: 
-    pop = np.random.uniform(-1, 1, (pop_size, agent_variables))
-    return pop
+    def create_random_rulebook(self):
+        
 
-
-# initializes environment with ai player using random controller, playing against static enemy
-#env = Environment(experiment_name=experiment_name,solutions = None)
-pop = init_pop(10,env)
-print(len(pop))
-
-#env.play()
-
-# set time marker
-# ini = time.time()
-
-# define parameters
-n_hidden = 10
-
-## n_vars = 265 in opt_spec_demo.py =
-# run simulation
-# normalize (to avoid negative probabilities of survival)
-# evaluation (calculates performance of every agent)
-# tournament (pick 2 players, keep the fittest aka natural selection)
-# limits ?
-# crossover (recombination & mutation)
-# kill worst genomes > replace with new best/random solutions
+    def control(self,inputs,controller):
+        # binarize inputs : 
+        
