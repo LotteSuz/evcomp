975e9e9ce7e457ea93caaca16d5db671c7a26fec
Lotte Heek <lotteheek@gmail.com>
2019-10-15 11:50:31 +0200

    200-fitness

--- A2.py
+++ A2.py
@@ -2,7 +2,7 @@
 import sys, os
 sys.path.insert(0, 'evoman')
 from environment import Environment
-from controller_lau1 import controller_automata
+from demo_controller import player_controller
 from itertools import product
 from functools import reduce
 import time
@@ -21,14 +21,14 @@ from joblib import Parallel, delayed
 
 
 # make folder for results
-experiment_name = 'EA2'
-if not os.path.exists(experiment_name):
-    os.makedirs(experiment_name)
+#experiment_name = 'EA2'
+#if not os.path.exists(experiment_name):
+#    os.makedirs(experiment_name)
 
 # initializes environment with ai player using self-made controller, playing against static enemy
 env = Environment(experiment_name='saturday',
-                  enemies=[2],
-                  player_controller=controller_automata(),
+                  #enemies=[2,3,4],
+                  player_controller=player_controller(),
                   level=2,
                   speed="fastest")
 
@@ -37,19 +37,19 @@ env = Environment(experiment_name='saturday',
 ini = time.time()
 
 
-def create_random_rulebook():
-    """Initializes a genotype for one agent, returns this agent a dictionary."""
-    t0 = time.time()
-    rulebook = {}
-    # iterate over possible inputs
-    for i in range(1048575):
-        key = str(bin(i))[2:].zfill(20)
-        outputstring  = list(bin(np.random.randint(31))[2:].zfill(5))
-        # convert to list of ints
-        output = [int(i) for i in outputstring]
-        rulebook[key] = output
-    t1 = time.time()
-    return rulebook
+# def create_random_rulebook():
+#     """Initializes a genotype for one agent, returns this agent a dictionary."""
+#     t0 = time.time()
+#     rulebook = {}
+#     # iterate over possible inputs
+#     for i in range(1048575):
+#         key = str(bin(i))[2:].zfill(20)
+#         outputstring  = list(bin(np.random.randint(31))[2:].zfill(5))
+#         # convert to list of ints
+#         output = [int(i) for i in outputstring]
+#         rulebook[key] = output
+#     t1 = time.time()
+#     return rulebook
 
 
 def evaluate(pop):
@@ -57,33 +57,55 @@ def evaluate(pop):
     and returns a list of the fitness scores of each member of the population."""
     fitness = []
     player_live = []
+
     # simulate game for every agent in the population
     for agent in pop:
+        agent_fitness = []
+        agent_life = []
+
+        # enemy 1
+        env.update_parameter('enemies', [1])
+        f,p,e,t = env.play(pcont=agent)
+        agent_fitness.append(f)
+        agent_life.append(p)
+
+        # enemy 5
+        env.update_parameter('enemies', [5])
         f,p,e,t = env.play(pcont=agent)
         # remember fitness
-        fitness.append(f)
-        player_live.append(p)
+        agent_fitness.append(f)
+        agent_life.append(p)
+
+        # get average fitness and player life
+        fitness.append(np.average(agent_fitness))
+        player_live.append(np.average(agent_life))
     return fitness,player_live
 
 
+# def crossover(agent_x, agent_y): OLD
+#     """Takes in two agents, recombines their genes at a random point,
+#     and returns two offspring."""
+#     new_agent = {}
+#     new_agent_two = {}
+#     len_dict = len(list(agent_x.values()))
+#     keys = list(agent_x.keys())
+#     ind = np.random.randint(1,len_dict)
+#     # create first kid
+#     other_controls_first = list(agent_x.values())[:ind] + list(agent_y.values())[ind:]
+#     for i in range(len_dict):
+#         new_agent[keys[i]] = other_controls_first[i]
+#     # create second kid
+#     ind2 = np.random.randint(1,len_dict)
+#     other_controls_second = list(agent_x.values())[:ind] + list(agent_y.values())[ind:]
+#     for j in range(len_dict):
+#         new_agent_two[keys[j]] = other_controls_second[j]
+#     return new_agent, new_agent_two
+
 def crossover(agent_x, agent_y):
-    """Takes in two agents, recombines their genes at a random point,
-    and returns two offspring."""
-    new_agent = {}
-    new_agent_two = {}
-    len_dict = len(list(agent_x.values()))
-    keys = list(agent_x.keys())
-    ind = np.random.randint(1,len_dict)
-    # create first kid
-    other_controls_first = list(agent_x.values())[:ind] + list(agent_y.values())[ind:]
-    for i in range(len_dict):
-        new_agent[keys[i]] = other_controls_first[i]
-    # create second kid
-    ind2 = np.random.randint(1,len_dict)
-    other_controls_second = list(agent_x.values())[:ind] + list(agent_y.values())[ind:]
-    for j in range(len_dict):
-        new_agent_two[keys[j]] = other_controls_second[j]
-    return new_agent, new_agent_two
+     ind = np.random.randint(1,n_vars-2)
+     new_agent_first = np.concatenate((agent_x[:ind], agent_y[ind:]), axis=None)
+     new_agent_second = np.concatenate((agent_x[ind:], agent_y[:ind]), axis=None)
+     return new_agent_first, new_agent_second
 
 
 def order_to_fitness(fitness,pop):
@@ -144,23 +166,17 @@ def mutate(kids,n_flip,mutation):
     to_mutate = int(mutation*len(kids))
     mutated_kids = []
     # list of keys and possible outputs
-    keys = list(kids[0].keys())
-    outputs = list(product([0,1], repeat=5))
+    #keys = list(kids[0].keys())
+    #outputs = list(product([0,1], repeat=5))
     # to lists
-    outputs = [list(o) for o in outputs]
+    #outputs = [list(o) for o in outputs]
     for i in range(to_mutate):
         ind = np.random.randint(len(kids)-1)
         while ind in mutated_kids:
             ind = np.random.randint(len(kids)-1)
         for j in range(n_flip):
-            toflip = np.random.randint(n_rules)
-            random_out = np.random.randint(31)
-            # print("lengte van kids = ", len(kids))
-            # print("waarde toflip = ", toflip)
-            # print("waarde keys = ", keys[toflip])
-            # print("huidige kid = ", kids[ind][keys[toflip]])
-            # print("outputs = ", outputs[random_out])
-            kids[ind][keys[toflip]] = outputs[random_out]
+            toflip = np.random.randint(n_vars)
+            kids[ind][toflip] = np.random.uniform(-1,1)
         mutated_kids.append(ind)
     return kids
 
@@ -170,7 +186,7 @@ def crowding_selection(kids, pop, family):
     which individuals of the family survive. Returns the updated population."""
     # get fitnesses of all family members
     for fam in family:
-        performance, fitness = evaluate([kids[fam[0]],kids[fam[1]],pop[fam[2]],pop[fam[3]]])
+        performance, life = evaluate([kids[fam[0]],kids[fam[1]],pop[fam[2]],pop[fam[3]]])
         # compute intercompetition distances
         left = abs(performance[0] - performance[2]) + abs(performance[1] - performance[3])
         right = abs(performance[0] - performance[3]) + abs(performance[1] - performance[2])
@@ -188,18 +204,25 @@ def crowding_selection(kids, pop, family):
     return pop
 
 
-def evolve(n_point_mut,mutation):
+def evolve(n_point_mut,mutation,number_agents,number_gen):
+#def evolve(n_point_mut,mutation):
     """Does not take in arguments, runs the evolutionary algorithm and returns the best performing agent,
     a list of the best fitness of each generation and the fitnesses of the complete last generation."""
-    # define parameters
-    global n_pop, n_gen,n_rules
+    # define parameter
+    global n_pop, n_gen,n_vars
     #n_point_mut = 50
-    n_pop = 10
-    n_gen = 1
+    n_pop = int(number_agents)
+    n_gen = int(number_gen)
+    #n_pop = 10
+    #n_gen = 2
     #mutation = 0.2
-    n_rules = 1048575
+    n_hidden = 10
+    n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5
+    print("n_vars = ", n_vars)
     t0 = time.time()
-    pop = [create_random_rulebook() for i in range(n_pop)]
+    dom_u = 1
+    dom_l = -1
+    pop = np.random.uniform(dom_l, dom_u, (n_pop, n_vars))
     fitnesses = []
     best_fitness = []
     av_fitnesses = []
@@ -219,12 +242,13 @@ def evolve(n_point_mut,mutation):
         # reproduce
         kids, family = make_children(pop, fitness, n_pop)
         # mutate  children
-        kids = mutate(kids,n_point_mut,mutation)
+        kids = mutate(kids,int(n_point_mut),int(mutation))
         # select new generation
         pop = crowding_selection(kids,pop,family)
     fittest_ind = np.argmax(fitness)
+    fittest_value = max(fitness)
 
-    return [pop[fittest_ind],best_fitness,fitnesses,av_fitnesses,generation_lives]
+    return [fittest_value,pop[fittest_ind],best_fitness,fitnesses,av_fitnesses,generation_lives]
 
 
 # if __name__ == "__main__":
@@ -242,7 +266,7 @@ def evolve(n_point_mut,mutation):
 #     pkl.dump(fitness,open(result_out2,'wb'))
 if __name__ == "__main__":
     m = sys.argv[1]
-    m= eval(m.split()[0])
+    m = eval(m.split()[0])
     #frac_mut_list = 0.9#,.3,.9
     #frac_mut_list = m[0]
     #n_to_flip = m[1]
@@ -254,8 +278,9 @@ if __name__ == "__main__":
 
     #for i in range(9):
     #t1 = time.time()
-    individ,best,fitnesses,av_fitnesses, gen_lives = evolve(m[1],m[0])
-    print(av_fitnesses[0])
+    best_sol,best_individ,best,fitnesses,av_fitnesses, gen_lives = evolve(m[1],m[0],m[2],m[3])
+    #best_sol,best_individ,best,fitnesses,av_fitnesses, gen_lives = evolve(m[1],m[0])
+    print(200 - best_sol)
     #fitness_list.append(fitnesses)
     #lifes_list.append(gen_lives)
     #t2 = time.time()
