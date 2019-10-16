a7446bec46a76df6cfc625b58ac6b3a8259a6823
maarten <Maarten.w.j@gmail.com>
2019-10-16 13:38:07 +0200

    spotpy
    Toegeveogd

---
+++ spotpy_doen.py
@@ -0,0 +1,38 @@
+import spotpy
+from A2 import Algoritme2
+
+
+class spotpy_setup(object):
+  def __init__(self):
+
+    self.cmfmodel = Algoritme2()
+    self.params = [ 
+        #n_point_mut, mutation, number_agents, number_gen
+        spotpy.parameter.Uniform('PointMutations', 1, 266),
+        spotpy.parameter.Uniform('mutation', 0, 1),
+        spotpy.parameter.Uniform('popsize', 10, 11),
+        spotpy.parameter.Uniform('generations', 10, 11)
+    ]
+
+  def parameters(self):
+    return spotpy.parameter.generate(self.params)
+
+  def simulation(self, vector):
+    simulations = self.cmfmodel.evolve(vector=vector)
+    return simulations
+
+  def evaluation(self):
+    #         return self.cmfmodel.obtain_obs_vals(k_shrub = 0.65, k_urban = 0.8)
+    #         return self.cmfmodel.get_usgs_fsh()
+    return [200]
+
+  def objectivefunction(self, simulation, evaluation):
+    objectivefunction = -spotpy.objectivefunctions.rmse(evaluation, simulation)
+    return objectivefunction
+
+spotpy_setup = spotpy_setup()
+
+sampler = spotpy.algorithms.rope(spotpy_setup, dbname='fdsjk', dbformat='csv', parallel='mpi')
+results = []
+sampler.sample()
+results.append(sampler.getdata)
